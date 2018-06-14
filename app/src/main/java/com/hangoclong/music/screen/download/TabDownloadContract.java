onghoangnhatphuong4697/demo

Load all tracks from Local

app/src/main/java/com/framgia/music/screen/tabdownload/TabDownloadContract.java
@@ -1,0 +1,21 @@
+package com.framgia.music.screen.tabdownload;
+
+import android.content.Context;
+import com.framgia.music.data.model.Collection;
+import com.framgia.music.screen.BasePresenter;
+
+/**
+ * Created by Admin on 3/21/2018.
+ */
+
+public interface TabDownloadContract {
+
+    interface View {
+        void showAllTracksFromLocal(Collection collection);
+        void showNoTracks();
+    }
+
+    interface Presenter extends BasePresenter<View> {
+        void getTracksFromLocal(Context context);
+    }
+}

app/src/main/java/com/framgia/music/screen/tabdownload/TabDownloadPresenter.java
@@ -1,0 +1,49 @@
+package com.framgia.music.screen.tabdownload;
+
+import android.content.Context;
+import com.framgia.music.data.model.Collection;
+import com.framgia.music.data.repository.TrackRepository;
+import com.framgia.music.data.source.RequestDataCallback;
+
+/**
+ * Created by Admin on 3/21/2018.
+ */
+
+public class TabDownloadPresenter implements TabDownloadContract.Presenter{
+
+    private TabDownloadContract.View mView;
+    private TrackRepository mTrackRepository;
+     TabDownloadPresenter(TrackRepository trackRepository) {
+        mTrackRepository = trackRepository;
+    }
+
+    @Override
+    public void setView(TabDownloadContract.View view) {
+        mView = view;
+    }
+
+    @Override
+    public void onStart() {
+        //No-op
+    }
+
+    @Override
+    public void onStop() {
+        //No-op
+    }
+
+    @Override
+    public void getTracksFromLocal(Context context) {
+        mTrackRepository.getAllTracksFromLocal(context, new RequestDataCallback<Collection>() {
+            @Override
+            public void onSuccess(Collection data) {
+                mView.showAllTracksFromLocal(data);
+            }
+
+            @Override
+            public void onFail(Exception e) {
+                mView.showNoTracks();
+            }
+        });
+    }
+}

GHLong1997/IMusic_04

app/src/main/java/com/hangoclong/music/screen/download/TabDownloadContract.java
@@ -1,0 +1,21 @@
+package com.hangoclong.music.screen.download;
+
+import android.content.Context;
+import com.hangoclong.music.data.model.Collection;
+import com.hangoclong.music.screen.BasePresenter;
+
+/**
+ * Created by Admin on 3/21/2018.
+ */
+
+public interface TabDownloadContract {
+
+    interface View {
+        void showAllTracksFromLocal(Collection collection);
+        void showNoTracks();
+    }
+
+    interface Presenter extends BasePresenter<View> {
+        void getTracksFromLocal(Context context);
+    }
+}

app/src/main/java/com/hangoclong/music/screen/download/TabDownloadPresenter.java
@@ -1,0 +1,49 @@
+package com.hangoclong.music.screen.download;
+
+import android.content.Context;
+import com.hangoclong.music.data.model.Collection;
+import com.hangoclong.music.data.repository.TrackRepository;
+import com.hangoclong.music.data.source.RequestDataCallback;
+
+/**
+ * Created by Admin on 3/21/2018.
+ */
+
+public class TabDownloadPresenter implements TabDownloadContract.Presenter{
+
+    private TabDownloadContract.View mView;
+    private TrackRepository mTrackRepository;
+     TabDownloadPresenter(TrackRepository trackRepository) {
+        mTrackRepository = trackRepository;
+    }
+
+    @Override
+    public void setView(TabDownloadContract.View view) {
+        mView = view;
+    }
+
+    @Override
+    public void onStart() {
+        //No-op
+    }
+
+    @Override
+    public void onStop() {
+        //No-op
+    }
+
+    @Override
+    public void getTracksFromLocal(Context context) {
+        mTrackRepository.getAllTracksFromLocal(context, new RequestDataCallback<Collection>() {
+            @Override
+            public void onSuccess(Collection data) {
+                mView.showAllTracksFromLocal(data);
+            }
+
+            @Override
+            public void onFail(Exception e) {
+                mView.showNoTracks();
+            }
+        });
+    }
+}